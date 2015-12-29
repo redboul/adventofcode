84dad406a9a0843398a068d5c0459474aa39efc3
julien-reboul <julien.reboul@bonitasoft.com>
2015-12-29 13:09:16 +0100

    day4

---
+++ src/day4/day4-2.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+  let md5 = require('md5'),
+    input = 'yzbqklnj',
+    counter = 0;
+  while(!md5(input + counter).match('^000000')) {
+    counter++;
+  }
+  console.log(counter, md5(input + counter));
+}());
